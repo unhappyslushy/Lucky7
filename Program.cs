f68ca37b9a7974bef0d95de5a5a43a5b10a10e68
UnHappySlushy <jakobt2@learn.cssd.ab.ca>
2018-10-12 12:59:28 -0600

    commit all files

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lucky7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalNumberOfSevens = 0;
+            Random rng = new Random();
+
+            int dice1;
+            int dice2;
+
+            for(int i = 1; i <= 100; i++)
+            {
+                dice1 = rng.Next(1, 7);
+                dice2 = rng.Next(1, 7);
+
+                if(dice1 + dice2 == 7)
+                totalNumberOfSevens++;
+            }
+
+            Console.WriteLine("Out of 100 rolls, you rolled {0} sevens ", totalNumberOfSevens);
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # lucky7
+
+test
